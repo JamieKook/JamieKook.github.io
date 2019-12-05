aa57492ef819ca7deb4b31085820ce7ba624d410
Jamie <kookjc6@gmail.com>
2019-12-04 21:12:05 -0600

    added prottfolio info section with working buttons to and from

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,14 @@ $(function () {
 $('.popover-dismiss').popover({
     trigger: 'focus'
 })
+
+$(".weather").on("click", function(){
+  $("#port-desk").hide();
+  $("#port-more").removeAttr("hidden");
+  $("#port-more").show(); 
+})
+
+$("#back").on("click", function(){
+  $("#port-desk").show();
+  $("#port-more").hide(); 
+})
